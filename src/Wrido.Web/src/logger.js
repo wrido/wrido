cb2e8a3dc05d11195a8c543fc87b9006e88569a3
korla <johannes.bengtsson@raysearchlabs.com>
2018-01-27 22:35:53 +0100

    Improve logger

--- src/Wrido.Web/src/logger.js
+++ src/Wrido.Web/src/logger.js
@@ -1,4 +1,7 @@
 export const logger = store => next => action => {
-  console.log(`Action dispatched: ${action.type}`, action)
+  console.group(`Action dispatched: ${action.type}`);
+  console.log('Action', action);
+  console.log('State', store.getState());
+  console.groupEnd();
   return next(action);
 }
