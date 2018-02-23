372f338579a4129a85c9f34afec0b5c7847184ad
par.dahlman <par.dahlman@fellowdeveloper.se>
2018-02-23 17:03:49 +0100

    Replace SignalR middleware with epic
    This makes sense for:
    
    * Backend 'stream', as it returns an Observable
      that can be dispatched as actions directly
      instead of setting subscribing in our code
    
    * The input stream, as we want to throttle it
      before calling backend
    
    Relates to #3

--- src/Wrido/ClientApp/src/modules/root.js
+++ src/Wrido/ClientApp/src/modules/root.js
@@ -1,8 +1,11 @@
 import { combineEpics } from 'redux-observable';
 import { combineReducers } from 'redux';
 import { input, result } from './reducers';
+import { backendStreamEpic, inputEpic} from "./epics";
 
 export const rootEpic = combineEpics(
+  backendStreamEpic,
+  inputEpic
 );
 
 export const rootReducer = combineReducers({
--- src/Wrido/ClientApp/src/store.js
+++ src/Wrido/ClientApp/src/store.js
@@ -2,12 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './modules/root';
 import { createEpicMiddleware } from 'redux-observable';
 import { rootEpic } from './modules/root';
-import { connectToSignalR } from './connectToSignalR';
 import { createLogger } from 'redux-logger';
 
 const middleware = [
   createEpicMiddleware(rootEpic),
-  connectToSignalR,
   createLogger(),
 ];
 
