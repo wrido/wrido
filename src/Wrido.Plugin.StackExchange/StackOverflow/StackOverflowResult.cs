8e836e92d9859fd1925cf5f4d6305cf1716584ca
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2018-02-25 11:56:35 +0100

    Add StackOverflow logo

--- src/Wrido.Plugin.StackExchange/StackOverflow/StackOverflowResult.cs
+++ src/Wrido.Plugin.StackExchange/StackOverflow/StackOverflowResult.cs
@@ -1,6 +1,19 @@
-﻿namespace Wrido.Plugin.StackExchange.StackOverflow
+﻿using System;
+using Wrido.Resources;
+
+namespace Wrido.Plugin.StackExchange.StackOverflow
 {
   public class StackOverflowResult : StackExchangeResult
   {
+    private static readonly Image _wikiLogo = new Image
+    {
+      Uri = new Uri("/resources/wrido/plugin/stackexchange/resources/StackOverflow.png", UriKind.Relative),
+      Alt = "StackOverflow"
+    };
+
+    public StackOverflowResult()
+    {
+      Icon = _wikiLogo;
+    }
   }
 }
